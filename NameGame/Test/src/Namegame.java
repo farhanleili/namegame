1f7e11a1b714f3de671c401b73d0aabed490714d
Alexander Jonsson <alexander.alexanderjonsson.se>
2018-06-05 15:28:56 +0200

    Denna version använder bara en array

--- NameGame/Test/src/Namegame.java
+++ NameGame/Test/src/Namegame.java
@@ -4,23 +4,22 @@ public class Namegame {
 	public static void main(String[] args) {
 		System.out.println("Hej, Name game " );
 
-		String[] firstname = {"Alex" , "Farhan" } ;
-		String[] lastname = {"Jonsson" , "Leili" };
+		String[] firstname = {"Alex", "Jonsson" , "Farhan", "Leili" } ;
 
 		Random rand = new Random();
 
 		int randomNumber1 = rand.nextInt(firstname.length);
 		int randomNumber2 = rand.nextInt(firstname.length);
 		
-		System.out.println("Random name is: " + firstname[randomNumber1] + "  " + lastname[randomNumber2]);
+		System.out.println("Random name is: " + firstname[randomNumber1] + "  " + firstname[randomNumber2]);
 
-		if ( firstname[randomNumber1] == firstname[0] && lastname[randomNumber2] == lastname[0] || firstname[randomNumber1] == firstname[1] && lastname[randomNumber2] == lastname[1])
-		{
-			System.out.println("right");
+		if (randomNumber2==randomNumber1+1){
+			System.out.println("True");
 		}
-		else {
-			System.out.println("false");
-		}		
+		else
+		{
+			System.out.println("False");
+		}
 
 	}
 }

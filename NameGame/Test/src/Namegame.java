f6ea436836a586c0b4494e0c8054dee580f92911
Alexander Jonsson <alexander.alexanderjonsson.se>
2018-06-05 15:59:49 +0200

    added explanations

--- NameGame/Test/src/Namegame.java
+++ NameGame/Test/src/Namegame.java
@@ -4,15 +4,28 @@ public class Namegame {
 	public static void main(String[] args) {
 		System.out.println("Hej, Name game " );
 
-		String[] name = {"Alex", "Jonsson" , "Farhan", "Leili"} ;
+		//Create the list of names, notice that the index of every first name will be odd and the index of every last name will be even.
+		String[] name = {"Alex", "Jonsson" , "Farhan", "Leili", "Donald", "Trump"} ; 
 
+		//Create a new random object
 		Random rand = new Random();
-
-		int randomNumber1 = rand.nextInt(name.length);
-		int randomNumber2 = rand.nextInt(name.length);
 		
+		//Create the variables that will hold the random numbers
+		int randomNumber1, randomNumber2;
+		
+		//Generate the random numbers
+		do {
+		randomNumber1 = rand.nextInt(name.length);
+		}while (randomNumber1%2!=0); //Check if the random number is odd or even. If the number is odd try again
+		
+		do {
+		randomNumber2 = rand.nextInt(name.length);
+		}while(randomNumber2%2==0); //Check if the random number is odd or even. If the number is even, try again.
+		
+		//Print the randomly gfenerated name
 		System.out.println("Random name is: " + name[randomNumber1] + "  " + name[randomNumber2]);
 
+		//Check if the first and last names match.
 		if (randomNumber2==randomNumber1+1)
 			System.out.println("True");
 		else

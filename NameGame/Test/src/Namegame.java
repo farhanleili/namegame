7c78d48bf43666215bd8d002f67880605f00cdd0
Alexander Jonsson <alexander.alexanderjonsson.se>
2018-06-05 15:32:12 +0200

    refactor

--- NameGame/Test/src/Namegame.java
+++ NameGame/Test/src/Namegame.java
@@ -4,23 +4,19 @@ public class Namegame {
 	public static void main(String[] args) {
 		System.out.println("Hej, Name game " );
 
-		String[] firstname = {"Alex", "Jonsson" , "Farhan", "Leili" } ;
+		String[] name = {"Alex", "Jonsson" , "Farhan", "Leili"} ;
 
 		Random rand = new Random();
 
-		int randomNumber1 = rand.nextInt(firstname.length);
-		int randomNumber2 = rand.nextInt(firstname.length);
+		int randomNumber1 = rand.nextInt(name.length);
+		int randomNumber2 = rand.nextInt(name.length);
 		
-		System.out.println("Random name is: " + firstname[randomNumber1] + "  " + firstname[randomNumber2]);
+		System.out.println("Random name is: " + name[randomNumber1] + "  " + name[randomNumber2]);
 
-		if (randomNumber2==randomNumber1+1){
+		if (randomNumber2==randomNumber1+1)
 			System.out.println("True");
-		}
 		else
-		{
 			System.out.println("False");
-		}
-
 	}
 }
 

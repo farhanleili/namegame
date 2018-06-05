ee889c5b096f54013f44c2a1b11e39cc046a7655
Alexander Jonsson <alexander.jonsson@outlook.com>
2018-06-06 01:24:19 +0200

    added some stuff

--- NameGame/Test/src/Namegame.java
+++ NameGame/Test/src/Namegame.java
@@ -1,12 +1,31 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Namegame {
-	public static void main(String[] args) {
-		System.out.println("Hej, Name game " );
-
+	public static void main(String[] args) throws InterruptedException {
 		//Create the list of names, notice that the index of every first name will be odd and the index of every last name will be even.
 		String[] name = {"Alex", "Jonsson" , "Farhan", "Leili", "Donald", "Trump"} ; 
+		
+		//Print all the names
+		System.out.println("Namegame" );
+		for (int i = 0;i<name.length;i++) {
+			if (i<name.length-1)
+			System.out.println(name[i] +" "+ name[i+1]);
+		}
+	
+		Scanner reader = new Scanner(System.in); 
+		System.out.println("Run the names ho wmany times? ");
+		int n = reader.nextInt();
+		reader.close();
+		
+		
+		for (int i=0;i<n;i++) {
+		play(name);
+		Thread.sleep(4);
+		}
+	}
 
+	private static void play(String[] name) {
 		//Create a new random object
 		Random rand = new Random();
 		
@@ -23,13 +42,13 @@ public class Namegame {
 		}while(randomNumber2%2==0); //Check if the random number is odd or even. If the number is even, try again.
 		
 		//Print the randomly gfenerated name
-		System.out.println("Random name is: " + name[randomNumber1] + "  " + name[randomNumber2]);
+		System.out.print("Random name is: " + name[randomNumber1] + "  " + name[randomNumber2]);
 
 		//Check if the first and last names match.
 		if (randomNumber2==randomNumber1+1)
-			System.out.println("True");
+			System.out.println("\t\tTrue");
 		else
-			System.out.println("False");
+			System.out.println("\t\tFalse");
 	}
 }
 
